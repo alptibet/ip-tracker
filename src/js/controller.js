8d8547fab1dcb373c6ef1967f9c683df18006a7e
alptibet <a.tibet@yandex.ru>
2021-06-24 19:00:05 +0300

    resolving async func

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,11 +4,18 @@ import { addHandlerSubmit } from './views/viewAddHandlerSubmit.js';
 import { renderMap } from './views/viewRenderMap.js';
 import { renderInfo } from './views/viewRenderInfo.js';
 
+const controlGetOwnInfo = function () {
+  model.getOwnInfo();
+  const addr = model.ipAddress;
+  console.log(addr);
+};
+
 const controlSubmit = function (address) {
   model.getData(address);
 };
 
 const init = function () {
+  controlGetOwnInfo();
   viewInitializeMap.initializeMap();
   addHandlerSubmit(controlSubmit);
 };
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,24 @@
 const APIkey = 'at_fheesH5ez5EDUhR0XFdMNoAxuYoQy';
 
+export const ipAddress = '';
+
+export const getOwnInfo = async function () {
+  try {
+    const response = await fetch('http://api.ipify.org/?format=json');
+    const resolve = await response.json();
+    const addr = resolve.ip;
+    setOwnInfo(addr);
+  } catch (error) {
+    alert('Could not get your IP address');
+  }
+};
+
+const setOwnInfo = function (addr) {
+  console.log(addr);
+  ipAddress = addr;
+  console.log(ipAddress);
+};
+
 export const getData = async function (address) {
   console.log(address);
   try {
